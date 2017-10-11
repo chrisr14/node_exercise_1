7d944fc5f3b25e1a89b070b9b17aff997ce70c8a
chrisr14 <31845430+chrisr14@users.noreply.github.com>
2017-10-11 17:51:09 +0200

    node app
    min førte node app

---
+++ greeter.js
@@ -0,0 +1,4 @@
+var greetings = ["Howdy, partner!", "Ahoy, matey!", "Hello, sunshine!", "Bonjour", "What's shaking bacon", "Yoo-hoo!", "hello", "hi"];
+var randomIndex = Math.floor(Math.random()*greetings.length);
+
+console.log(greetings[randomIndex]);
